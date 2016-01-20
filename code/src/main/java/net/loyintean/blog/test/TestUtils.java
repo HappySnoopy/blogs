d6cc500f86907feaeae13dce8a66e2d6cc3e3b26
linjun <linjun@ucredit.com>
2016-01-20 23:19:39 +0800

    utils to test private methods

---
+++ code/src/main/java/net/loyintean/blog/test/TestUtils.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright(c) 2011-2016 by YouCredit Inc.
+ * All Rights Reserved
+ */
+package net.loyintean.blog.test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author linjun
+ */
+public class TestUtils {
+
+    public static final <T> T invokePrivateMethod(Object instance, String name,
+            Object... args) {
+
+        Class<?>[] clazArray = new Class<?>[args.length];
+
+        for (int i = 0; i < args.length; i++) {
+            clazArray[i] = args[i].getClass();
+        }
+
+        try {
+            Method method = instance.getClass().getDeclaredMethod(name,
+                clazArray);
+            method.setAccessible(true);
+
+            T result = (T) method.invoke(instance, args);
+            return result;
+
+        } catch (NoSuchMethodException | SecurityException
+                | IllegalAccessException | InvocationTargetException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final <T> T invokePrivateMethod(Object instance, String name) {
+
+        try {
+            Method method = instance.getClass().getDeclaredMethod(name);
+            method.setAccessible(true);
+
+            T result = (T) method.invoke(instance);
+            return result;
+
+        } catch (NoSuchMethodException | SecurityException
+                | IllegalAccessException | InvocationTargetException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+}
