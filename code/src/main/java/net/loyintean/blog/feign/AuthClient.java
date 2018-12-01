dcb18c99ef72d09bab7218fc78b104984ee7c69c
junlin8 <junlin@creditease.cn>
2018-12-01 16:30:03 +0800

    爬取IP138数据的单线程版本

--- code/src/main/java/net/loyintean/blog/feign/AuthClient.java
+++ code/src/main/java/net/loyintean/blog/feign/AuthClient.java
@@ -3,16 +3,15 @@
  */
 package net.loyintean.blog.feign;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import rx.Observable;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author linjun
@@ -23,20 +22,18 @@ public interface AuthClient {
 
     @RequestMapping(method = RequestMethod.GET,
             value = "/thread_auth/rest/userInfos/vague")
-    public List<Map<String, Object>> getUserInfosByNameVague(
-            @RequestParam("name") String name,
-            @RequestParam("title") String title,
-            @RequestParam("enabled") Boolean enabled,
-            @RequestHeader(name = "Authorization",
-                    defaultValue = "Basic YWRtaW46c2VjMDA3dXjpdHk=") String authrization);
+    List<Map<String, Object>> getUserInfosByNameVague(@RequestParam("name") String name,
+                                                      @RequestParam("title") String title,
+                                                      @RequestParam("enabled") Boolean enabled,
+                                                      @RequestHeader(name = "Authorization",
+                                                              defaultValue = "Basic YWRtaW46c2VjMDA3dXjpdHk=") String authrization);
 
     @RequestMapping(method = RequestMethod.GET,
             value = "/thread_auth/rest/userInfos/vague")
-    public Observable<List<Map<String, Object>>> getUserInfosByNameVague_toObservable(
-            @RequestParam("name") String name,
-            @RequestParam("title") String title,
-            @RequestParam("enabled") Boolean enabled,
-            @RequestHeader(name = "Authorization",
-                    defaultValue = "Basic YWRtaW46c2VjMDA3dXjpdHk=") String authrization);
+    Observable<List<Map<String, Object>>> getUserInfosByNameVague_toObservable(@RequestParam("name") String name,
+                                                                               @RequestParam("title") String title,
+                                                                               @RequestParam("enabled") Boolean enabled,
+                                                                               @RequestHeader(name = "Authorization",
+                                                                                       defaultValue = "Basic YWRtaW46c2VjMDA3dXjpdHk=") String authrization);
 
 }
---
+++ code/src/main/java/net/loyintean/blog/ip138/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * ip138的简单爬虫
+ */
+package net.loyintean.blog.ip138;
